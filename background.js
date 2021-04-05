ac5b6ab698f4fc642f6f176d32443eb5b20f55b5
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-04-05 18:52:11 +0200

    feat: add dynamic filter property detection based on Fx version refactor: cleanup

--- background.js
+++ background.js
@@ -1,68 +1,53 @@
 
-const extId = "stopOpenByRegEx";	
-const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+(async () => {
+	const manifest = browser.runtime.getManifest();
+	const extId = manifest.name;
 
-function log(level, msg) {
-	level = level.trim().toLowerCase();
-	if (['error','warn'].includes(level)
-		|| ( temporary && ['debug','info','log'].includes(level))
-	) {
-		console[level](extId + '::' + level.toUpperCase() + '::' + msg);
-	}
-};
+	browser.tabs.onUpdated.addListener(async function (tabId, changeInfo, tabInfo) {
 
-
-async function onUpdated(tabId, changeInfo, tabInfo) {
-
-	if(typeof changeInfo.url !== 'string'){
-		return;
-	}
-	if( ! /^https?:\/\//.test(changeInfo.url)) {
-		return;
-	}
-	//log('debug','changeInfo.url := ' + changeInfo.url);
-
-	const selectors = await ((async () => {
-		try {
-			const tmp = await browser.storage.local.get('selectors');
-			if(typeof tmp['selectors'] !== 'undefined') {
-				return tmp['selectors'];
-			}
-		}catch(e){
+		if(typeof changeInfo.url !== 'string' 
+			|| !(new RegExp("^https?:\/\/")).test(changeInfo.url) ) {
+			return;
 		}
-		return [];
-	})());
 
-	for(const selector of selectors) {
-
-		// check activ
-		if(typeof selector.activ === 'boolean') { 
-			if(selector.activ === true) { 
-
-				// check url regex 
-				if(typeof selector.url_regex === 'string') { 
-					selector.url_regex = selector.url_regex.trim();
-					if(selector.url_regex !== ''){ 
-
-						if((new RegExp(selector.url_regex)).test(tabInfo.url) ){
-							await browser.tabs.remove(tabId);
-							browser.notifications.create(extId, {
-								"type": "basic",
-								"iconUrl": browser.runtime.getURL("icon.png"),
-								"title": extId,
-								"message":  'closed tab, RegularExpression: ' + selector.url_regex + '\n matched with target url: ' + tabInfo.url
-							});
-							return; 
-						}
-					}
+		const selectors = await ((async () => {
+			try {
+				const tmp = await browser.storage.local.get('selectors');
+				if(typeof tmp['selectors'] !== 'undefined') {
+					return tmp['selectors'];
 				}
+			}catch(e){
+				console.error(e);
+			}
+			return [];
+		})());
+
+		for(const selector of selectors) {
+
+			try {
+				if(typeof selector.activ === 'boolean'
+					&& selector.activ === true
+					&& typeof selector.url_regex === 'string'
+					&& selector.url_regex !== ''
+					&& (new RegExp(selector.url_regex)).test(tabInfo.url) 
+				){
+					browser.tabs.remove(tabId);
+					browser.notifications.create(extId + tabId, {
+						"type": "basic",
+						"iconUrl": browser.runtime.getURL("icon.png"),
+						"title": extId,
+						"message":  'closed tab, RegularExpression: ' + selector.url_regex + '\n matched with target url: ' + tabInfo.url
+					});
+					return; 
+				}
+			}catch(e){
+				console.error(e);
 			}
 		}
-	}
-}
-
-const filter = {
-	properties: ['status'] // TODO: after Fx 88 use 'url' instead of status 
-}
-
-browser.tabs.onUpdated.addListener(onUpdated /**/, filter/**/);
+	}, // filter 
+		(await (async  () => {
+		const info = await browser.runtime.getBrowserInfo();
+		const major_version = info.version.split(".")[0];
+		return { properties: [((major_version < 88)?'status':'url')] };
+	})()));
+})();
